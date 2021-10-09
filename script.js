1796ddfb22e9fb7433628b2487597c1d802c8eab
Serenely <shilovboris12@gmail.com>
2021-10-09 20:51:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const category = 'toys';
+
+console.log(`https://someth.com/${category}/5`);
+
+
+let person = prompt("Ваше имя?", '');
+
+document.write(`Я тебя люблю ${person}!`);
+
+
+console.log(9*2 == '18');
+
+const isChecked = false,
+       isClose = false;
+
+       console.log(!isChecked || isClose);
